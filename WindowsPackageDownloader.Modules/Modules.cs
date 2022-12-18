4615c5f7669147e40d36df41de16f7bb53c1dde2
Luca <lucapols51@gmail.com>
2022-12-18 14:14:42 +0100

    1new

---
+++ WindowsPackageDownloader.Modules/Modules.cs
@@ -0,0 +1,18 @@
+﻿namespace WindowsPackageDownloader.Modules
+{
+    public class Modules
+    {
+        public static void GetIfVersionAvailable(string Version)
+        {
+            // get from database
+
+            // if not available, return error
+
+            // if error, return error
+
+            // if success, return success
+            
+            
+        }
+    }
+}
