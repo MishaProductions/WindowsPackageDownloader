2b65c57307159cef03b742cbf23520c9ace068f9
Misha <46088515+MishaTY@users.noreply.github.com>
2022-12-18 07:38:08 -0500

    website

---
+++ WindowsPackageDownloader/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WindowsPackageDownloader.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
--- WindowsPackageDownloader/Program.cs
+++ WindowsPackageDownloader/Program.cs
@@ -1,4 +1,3 @@
-
 namespace WindowsPackageDownloader
 {
     public class Program
@@ -8,27 +7,28 @@ namespace WindowsPackageDownloader
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (!app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
 
             app.UseAuthorization();
 
-
-            app.MapControllers();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
