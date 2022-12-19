5d02ff33f398da39ff7dc738d3d91c37d014064d
Misha <46088515+MishaTY@users.noreply.github.com>
2022-12-19 11:26:25 -0500

    fetch updates

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -8,7 +8,7 @@ namespace TestApp
         {
             Console.WriteLine("Hello, World!");
             var client = new WUClient();
-            await client.FetchVersionInfo();
+            await client.FetchUpdate("All", "Retail", "Active", 20348, 1, 8);
             Console.Write("Press any key to exit . . .");
             Console.ReadKey();
         }
